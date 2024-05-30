6d491b456b8c41fbfeec87703d63d2c5b8bb7d80
Hongliang <hongliang@Hongliangs-MacBook-Pro.local>
2024-05-30 11:22:03 +0800

    init push

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Begin!");
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine("Print" + i.ToString());
+}
+Console.WriteLine("End!");
